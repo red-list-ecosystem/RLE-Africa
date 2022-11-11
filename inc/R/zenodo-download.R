eeb6e643aac525255d1b91dec58206c9b0f0d9d3
jrfep <jr.ferrer.paris@gmail.com>
2022-11-11 11:39:24 +1100

    Scripts for download with zen4R from an R session

--- inc/R/zenodo-download.R
+++ inc/R/zenodo-download.R
@@ -1,8 +1,14 @@
 #R --vanilla
+####
+## IUCN Red List of Ecosystems in Africa
+## J.R. Ferrer-Paris https://github.com/jrfep
+## Script to download of data from a Zenodo repository
+####
+
 require(zen4R)
 library(parallel)
 setwd("Data")
-#Version 2.1.1 
+#Version 2.1.1
 doi = "10.5281/zenodo.5090419"
 
 download_zenodo(doi=doi, parallel = TRUE, parallel_handler = parLapply, cl = makeCluster(2))
@@ -16,6 +22,6 @@ rec$downloadFiles(path = "Data/")
 #OR:
 #cd Data/
 #wget --continue https://zenodo.org/api/files/4991f687-63f1-4ec8-ab92-75c8fcd5bd6a/all-maps-raster-geotiff.tar.bz2
-#tar -xjvf all-maps-raster-geotiff.tar.bz2 
+#tar -xjvf all-maps-raster-geotiff.tar.bz2
 
 q()
