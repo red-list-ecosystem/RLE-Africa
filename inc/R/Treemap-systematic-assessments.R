cbed3beb543b48e0c45ea79bf4d69797c706eaaf
Jose Rafael Ferrer-Paris <jr.ferrer.paris@gmail.com>
2023-07-21 22:12:04 +1000

    Figures and table for revised version of the manuscript

--- inc/R/Treemap-systematic-assessments.R
+++ inc/R/Treemap-systematic-assessments.R
@@ -1,175 +1,338 @@
 # Set up ----
 
 ## libraries
-require(sf)
 require(dplyr)
-library(readxl)
-library(stringr)
-#require(foreign)
-#require(units)
-#require(magrittr)
-require(tidyr)
 library(treemapify)
 library(ggplot2)
 library(readr)
+library(cowplot)
+library(stringr)
 
 ## working directory
 here::i_am("inc/R/Treemap-systematic-assessments.R")
 
 
-## Read tables ----
-WIO <- read_csv(here::here("Data", "systematic-assessment-summaries", 
-                           "WIO-summary-RLE.csv"))
-mada <- read_csv(here::here("Data", "systematic-assessment-summaries",
-                            "madagascar-summary-RLE.csv"))
-macaronesia <- read_csv(here::here("Data", "systematic-assessment-summaries",
-                            "macaronesia-summary-RLE.csv"))
-congo <- read_csv(here::here("Data", "systematic-assessment-summaries",
-                            "congo-summary-RLE.csv"))
-mozambique <- read_csv(here::here("Data", "systematic-assessment-summaries",
-                            "mozambique-summary-RLE.csv"))
-
-
 ## Colours ----
+
+# Tested this colour scheme with 
+# https://pilestone.com/pages/color-blindness-simulator-1
 
 clrs <- c(
   "NE" = "white",
-  "DD" = "grey",
-  "LC" = "green4",
-  "NT" = "green",
-  "VU" = "yellow",
+  "DD" = "grey45",
+  "LC" = "palegreen4",
+  "NT" = "honeydew3",
+  "VU" = "yellow3",
   "EN" = "orange",
-  "CR" = "red")
-
-## Plots ----
-
-ggplot(macaronesia, 
-       aes(area=`area`, fill = category, label = code,
-           subgroup = efg_code)) +
-  geom_treemap() +
-  geom_treemap_subgroup_text(
-    place = "bottomleft", 
-    grow = F, 
-    alpha = 0.35, 
-    #colour = thm_clrs[1], 
-    fontface = "italic", 
-    min.size = 0) +
-  geom_treemap_subgroup_border() +
-  scale_fill_manual(values=clrs) +
-  labs(
-    title = "Ecosystems of Macaronesia (Canary Island and Madeira)",
-    subtitle='Each box is an assessment unit\ngrouped by biome or ecosystem functional groups.', fill='Risk category')
-
-ggsave(here::here("Output", "Treemap-Example-terrestrial-Macaronesia.png"))
-
-
-## South Africa ----
-
-ggplot(southafrica, aes(area=estimated_area, fill = category, label = NAME,
-               subgroup = efg_code)) +
-  geom_treemap() +
-  geom_treemap_subgroup_text(
-    place = "bottomleft", 
-    grow = F, 
-    alpha = 0.35, 
-    #colour = thm_clrs[1], 
-    fontface = "italic", 
-    min.size = 0) +
-  geom_treemap_subgroup_border() +
-  scale_fill_manual(values=clrs) +
-  labs(
-    title = "Terrestrial ecosystems in South Africa",
-    subtitle='Each box is an assessment unit\ngrouped by ecosystem functional groups.', fill='Risk category')
-
-ggsave(here::here("Output", "Treemap-Example-terrestrial-South-Africa.png"))
+  "CR" = "red4")
 
 
 
-## WIO ----
+## Functions ----
 
-# Fix needed: data is missing...
-
-ggplot(WIO, aes(area=area_km2, fill = category, label = eco_name,
-                 subgroup = efg_code)) +
-  geom_treemap() +
-  geom_treemap_subgroup_text(
-    place = "bottomleft", 
-    grow = F, 
-    alpha = 0.35, 
-    #colour = thm_clrs[1], 
-    fontface = "italic", 
-    min.size = 0) +
-  geom_treemap_subgroup_border() +
-  scale_fill_manual(values=clrs) +
-  labs(
-    title = "Coral reefs of Western Indian Ocean",
-    subtitle='Each box is an assessment unit\ngrouped by biome or ecosystem functional groups.', fill='Risk category')
-
-ggsave(here::here("Output", "Treemap-Example-terrestrial-WIO.png"))
-
-## Madagascar ----
-
-ggplot(mada, aes(area=area_km2, fill = category, label = eco_name,
-                subgroup = efg)) +
-  geom_treemap() +
-  geom_treemap_subgroup_text(
-    place = "bottomleft", 
-    grow = F, 
-    alpha = 0.35, 
-    #colour = thm_clrs[1], 
-    fontface = "italic", 
-    min.size = 0) +
-  geom_treemap_subgroup_border() +
-  scale_fill_manual(values=clrs) +
-  labs(
-    title = "Ecosystems of Madagascar",
-    subtitle='Each box is an assessment unit\ngrouped by biome or ecosystem functional groups.', fill='Risk category')
-
-ggsave(here::here("Output", "Treemap-Example-terrestrial-Madagascar.png"))
+treemap_plot <- function(X) {
+  required_cols <- c("assessment_area", "category", "eco_name", "efg_code")
+  stopifnot(required_cols %in% colnames(X))
+  p <- ggplot(X, 
+              aes(area=assessment_area, 
+                  fill = category, 
+                  label = eco_name,
+                  subgroup = efg_code)) +
+    geom_treemap(colour="black") +
+    geom_treemap_subgroup_text(
+      place = "bottomleft", 
+      grow = FALSE, 
+      alpha = 0.85, 
+      colour = "whitesmoke", 
+      fontface = "italic", 
+      min.size = 0) +
+    geom_treemap_subgroup_border(colour="black") +
+    scale_fill_manual(values=clrs)
+  return(p)
+}
 
 
+## Read tables ----
+
+lookup <- c(eco_name = c("code", "Name", "NAME", "Ecosystem_Primary"), 
+            assessment_area = 
+              c("area", "Area_ha", "area_km2", "estimated_area"),
+            efg_code = "efg",
+            category = "overall_risk_category")
+
+
+WIO <- read_csv(here::here("Data", "systematic-assessment-summaries", 
+                           "WIO-summary-RLE.csv")) %>% 
+  rename(any_of(lookup)) %>% rename_with(~str_replace(.x,"[0-9]$",""))
+
+madagascar <- read_csv(here::here("Data", "systematic-assessment-summaries",
+                            "madagascar-summary-RLE.csv")) %>% 
+  rename(any_of(lookup)) %>% rename_with(~str_replace(.x,"[0-9]$",""))
+
+macaronesia <- read_csv(here::here("Data", "systematic-assessment-summaries",
+                            "macaronesia-summary-RLE.csv")) %>% 
+  rename(any_of(lookup)) %>% rename_with(~str_replace(.x,"[0-9]$",""))
+
+congo <- read_csv(here::here("Data", "systematic-assessment-summaries",
+                            "congo-summary-RLE.csv")) %>% 
+  rename(any_of(lookup)) %>% rename_with(~str_replace(.x,"[0-9]$",""))
+
+mozambique <- read_csv(here::here("Data", "systematic-assessment-summaries",
+                            "mozambique-summary-RLE.csv")) %>% 
+  rename(any_of(lookup)) %>% rename_with(~str_replace(.x,"[0-9]$",""))
+
+southafrica <- read_csv(here::here("Data", "systematic-assessment-summaries",
+                                   "south-africa-summary-RLE.csv"))%>% 
+  rename(any_of(lookup)) %>% rename_with(~str_replace(.x,"[0-9]$",""))
+
+southafrica_mar <- read_csv(here::here("Data", "systematic-assessment-summaries",
+                                   "south-africa-marine-summary-RLE.csv"))%>% 
+  rename(any_of(lookup)) %>% rename_with(~str_replace(.x,"[0-9]$",""))
+
+
+## Main systematic assessments; plots for MS ----
+
+### All together ----
+
+all_data <- bind_rows(southafrica,mozambique,madagascar,congo) %>% 
+  filter(!is.na(assessment_area)) %>%
+  mutate(efg_code=str_extract(efg_code,"[A-Z0-9]+"),
+         category = if_else(category %in% "NE","DD",category),
+         category = factor(category, levels=names(clrs)))
+
+all_plot <- treemap_plot(all_data) +
+  labs(fill = "Risk category")
+
+### Madagascar ----
+
+other_efgs <- c("T3.1", "FM1")
+other_efgs <- c("F2","F1","FM1", "T3.4", "TF1.1", "MFT1.1", "MFT1.2", "MT2.1", "M1.1", "T3.1")
+mada_data <- madagascar %>% 
+  filter(!is.na(assessment_area)) %>%
+  mutate(
+    category = if_else(category %in% "NE","DD",category),
+    efg_code = if_else(efg_code %in% other_efgs, "others: T3.4, TF1, F1, F2, FM1, MFT1, MT1, M1.1, T3.1", efg_code),
+    efg_code = str_wrap(efg_code,18)) 
+
+mada_plot <- treemap_plot(mada_data) +
+  labs(title = "Ecosystems of Madagascar") + theme(legend.position = "none")
+
+mada_plot +
+  labs(subtitle='Each box is an assessment unit\ngrouped by biome or ecosystem functional groups.', fill='Risk category')
 
 ### Congo ----
 
+other_efgs <- c("T1.3", "MFT1.2", "TF1.1")
 
-ggplot(congo, aes(area=Area_ha, fill = overall_risk_category, label = eco_name,
-                 subgroup = efg_code)) +
-  geom_treemap() +
-  geom_treemap_subgroup_text(
-    place = "bottomleft", 
-    grow = F, 
-    alpha = 0.35, 
-    #colour = thm_clrs[1], 
-    fontface = "italic", 
-    min.size = 0) +
-  geom_treemap_subgroup_border() +
-  scale_fill_manual(values=clrs) +
-  labs(
-    title = "Ecosystems of the Congo basin",
-    subtitle='Each box is an assessment unit\ngrouped by biome or ecosystem functional groups.', fill='Risk category')
 
-ggsave(here::here("Output", "Treemap-Example-terrestrial-Congo.png"))
+congo_data <- congo %>%
+  mutate(efg_code = if_else(efg_code %in% other_efgs, "others: TF1.1, T1.3, MFT1.2", efg_code),
+         efg_code = str_wrap(efg_code,12))
+
+
+congo_plot <- treemap_plot(congo_data) +
+  labs(title = "Congo basin") + theme(legend.position = "none")
+
+congo_plot
 
 ### Mozambique ----
+mft_efgs <- c("MFT", "MFT1.1", "MFT1.2", "MFT1.3")
+other_efgs <- c("MT1.3", "T1.3", "MT2.1","T4.5", 
+                "F2.2", "F2.3", "F1.5", "T3.1", "T1.1")
+mozambique_data <- mozambique %>% 
+  mutate(efg_code = case_when(
+    efg_code %in% other_efgs ~ "others: T1.1, T1.3, T3.1, T4.5, F2, F1, MT1, MT2",
+    efg_code %in% mft_efgs ~ "MFT1",
+    TRUE ~ efg_code),
+    efg_code = str_wrap(efg_code,18))
+
+moz_plot <- treemap_plot(mozambique_data) +
+  labs(title = "Mozambique") + theme(legend.position = "none")
+
+moz_plot
+
+### South Africa ----
+
+zaf_data <- southafrica %>% 
+  filter(!is.na(assessment_area))
+
+zaf_data %>% 
+  group_by(efg_code) %>% 
+  summarise(total=sum(assessment_area)) %>% 
+  arrange(total)
+
+other_efgs <- c("T1.2", "T1.3", "T2.3", "T2.4",  
+                "T3.1", "T4.4", "T5.5", 
+                "MT2.1", "MFT1.2")
+
+zaf_data <- zaf_data %>% 
+  mutate(
+    efg_code = case_when(
+      efg_code %in% other_efgs ~ 
+        "others: T1.2, T1.3, T2.3, T2.4, T3.1, T4.4, T5.5, MT2.1, MFT1",
+      TRUE ~ efg_code),
+    efg_code = str_wrap(efg_code,18))
+
+zaf_plot <- treemap_plot(zaf_data) +
+  labs(title = "South Africa") + theme(legend.position = "none")
+zaf_plot
 
 
-ggplot(mozambique, aes(area=area_km, fill = category, label = eco_name,
-                subgroup = efg_code)) +
-  geom_treemap() +
-  geom_treemap_subgroup_text(
-    place = "bottomleft", 
-    grow = F, 
-    alpha = 0.35, 
-    #colour = thm_clrs[1], 
-    fontface = "italic", 
-    min.size = 0) +
-  geom_treemap_subgroup_border() +
-  scale_fill_manual(values=clrs) +
+
+### Create and export grid of plots ----
+
+pgrid <- plot_grid(
+  congo_plot + labs(title = element_blank()), 
+  mada_plot + labs(title = element_blank()),
+  zaf_plot + labs(title = element_blank()), 
+  moz_plot + labs(title = element_blank()),
+  labels = c("A", "B", "C","D"),
+  hjust = -1,
+  vjust=3,
+  greedy = TRUE
+)
+
+# extract a legend that is laid out horizontally
+legend_b <- get_legend(
+  all_plot + 
+    guides(fill = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom")
+)
+plot_grid(pgrid, legend_b, ncol = 1, rel_heights = c(1, .1))
+
+ggsave(here::here("Output", "Treemap-Example-4-assessments.png"),
+       width = 7.5, height = 7)
+
+
+## Preliminary systematic assessments for Supplement ----
+
+### Macaronesia ----
+
+other_efgs <- c("T4.5","F2.2", "TF1.4","F2.3", "MTF1.3")
+macaronesia_data <- macaronesia %>% 
+  mutate(efg_code = if_else(efg_code %in% other_efgs, "others: MFT1, F2, TF1, T4.5", efg_code),
+         efg_code = str_wrap(efg_code,18))
+
+macaronesia_plot <- treemap_plot(macaronesia_data) 
+
+macaronesia_plot +
+  theme_cowplot(12) +
   labs(
-    title = "Ecosystems of Mozambique",
-    subtitle='Each box is an assessment unit\ngrouped by biome or ecosystem functional groups.', fill='Risk category')
+    title = "Ecosystems of Macaronesia (Canary Island and Madeira)",
+    subtitle='Each tile is an assessment unit, size proportional to area\ngrouped by biome or ecosystem functional groups.', fill='Risk category')
 
-ggsave(here::here("Output", "Treemap-Example-terrestrial-Mozambique.png"))
+ggsave(here::here("Output", "Treemap-Example-terrestrial-Macaronesia.png"))
+
+### WIO ----
+
+WIO_plot <- treemap_plot(WIO) 
+WIO_plot +
+  labs(title = "Coral reefs of Western Indian Ocean",subtitle='Each box is an assessment unit\ngrouped by biome or ecosystem functional groups.', fill='Risk category')
+
+### South Africa Marine ----
+
+zafm_data <- southafrica_mar %>% 
+  filter(!is.na(assessment_area))
+
+zafm_data %>% 
+  group_by(efg_code) %>% 
+  summarise(total=sum(assessment_area)) %>% 
+  arrange(total)
+
+mts_efgs <- c("MISSING")
+
+other_efgs <- c("MT1.1", 
+                "MT1.3","M3.4",
+                "MT1.4","M1.3", 
+                "M1.6","FM1.2", 
+                "M3.2", "M1.2")
 
 
+zafm_data <- zafm_data %>% 
+  mutate(
+    efg_code = case_when(
+      efg_code %in% mts_efgs ~ 
+        "unclassified",
+      efg_code %in% other_efgs ~ 
+        "others: MT1, M1.2, M1.3, M1.6, M3.2, M3.4, FM1.2",
+      TRUE ~ efg_code),
+    efg_code = str_wrap(efg_code,18))
+
+zafm_plot <- treemap_plot(zafm_data)  + theme(legend.position = "none")
+zafm_plot +
+  labs(title = "South Africa")
+
+
+
+### Create and export grid of plots ----
+# extract a legend that is laid out vertically
+legend_c <- get_legend(
+  all_plot + 
+    guides(fill = guide_legend(ncol = 1)) +
+    theme(legend.position = "right")
+)
+
+pgrid <- plot_grid(
+  zafm_plot,
+  macaronesia_plot + theme(legend.position = "none"),
+  WIO_plot + theme(legend.position = "none"), 
+   legend_c,
+  labels = c("A", "B", "C"),
+  hjust = -1,
+  vjust=2,
+  greedy = TRUE
+)
+
+pgrid
+
+ggsave(here::here("Output", "Treemap-preliminary-assessments.png"),
+       width = 7.5, height = 7)
+
+
+
+
+
+## Export output plots ----
+
+ggsave(moz_plot,
+       filename = 
+         here::here("Output", "Treemap-Example-terrestrial-Mozambique.png"))
+ggsave(congo_plot,
+       filename = 
+         here::here("Output", "Treemap-Example-terrestrial-Congo.png"))
+ggsave(zaf_plot,
+       filename = 
+         here::here("Output", "Treemap-Example-terrestrial-South-Africa.png"))
+ggsave(WIO_plot,
+       filename = 
+         here::here("Output", "Treemap-Example-terrestrial-WIO.png"))
+
+ggsave(mada_plot,
+       filename = 
+         here::here("Output", "Treemap-Example-terrestrial-Madagascar.png"))
+
+
+
+all_data <- 
+  bind_rows(southafrica %>% mutate(assessment="South Africa (terrestrial)"),
+            mozambique %>% mutate(assessment="Mozambique"),
+            madagascar %>% mutate(assessment="Madagascar"), 
+            congo %>% mutate(assessment="Congo basin")) %>%
+  bind_rows(southafrica_mar %>% mutate(assessment="South Africa (marine)"), 
+            macaronesia %>% 
+              mutate(eco_name= if_else(is.na(Habitat_na),
+                                       paste(`Habitat Type Name`),
+                                       paste(eco_name, Habitat_na)),
+                     assessment="Macaronesia"), 
+            WIO %>% mutate(assessment="Western Indian Ocean")) %>%
+  group_by(assessment, eco_name) %>%
+  summarise(efg_codes = paste(unique(efg_code), collapse = ";"),
+            assessment_area = max(assessment_area),
+            category = first(category),
+            .groups="keep")
+
+write_csv(
+  all_data,
+  here::here("Data", "systematic-assessment-summaries",
+           "Supplementary-table-systematic-assessment-summary.csv"))
 
--- inc/R/read-South-Africa-data.R
+++ inc/R/read-South-Africa-data.R
@@ -3,17 +3,13 @@ require(sf)
 require(dplyr)
 library(readxl)
 library(stringr)
-#require(foreign)
-#require(units)
-#require(magrittr)
 require(tidyr)
-#library(readr)
+library(readr)
 
 ## working directory
 here::i_am("inc/R/read-South-Africa-data.R")
 
 
-# Fix needed: data is missing...
 za <- read_sf(here::here("Data", "ZAF", "NBA2018_Terrestrial_ThreatStatus_ProtectionLevel.gdb"))
 za %>% slice(1) %>% select(RLEv5)
 
@@ -29,6 +25,20 @@ za_list <-
   summarise(category=paste(unique(RLEv5),collapse=";"), 
             mapped_area=sum(SA_Nat2014),
             .groups="keep")
+
+
+zam <- read_sf(here::here("Data", "ZAF", "NBA2018_Marine_ThreatStatus_ProtectionLevel.gdb"))
+zam %>% slice(1) 
+
+zam_list <- 
+  zam %>% 
+  st_drop_geometry() %>% 
+  filter(!is.na(BroadEcosystemGroup)) %>%
+  group_by(BroadEcosystemGroup,Ecosystem_Primary) %>% 
+  summarise(category=paste(unique(RLE_2018b),collapse=";"), 
+            mapped_area=sum(Shape_Area),
+            .groups="keep")
+
 
 xwalk_file <- here::here("Data", "ZAF", "GETcrosswalk_SouthAfricaTerrestrial_V2_17012020_DK.xlsx")
 
@@ -65,3 +75,36 @@ tbl <- za_list %>%
             estimated_area=sum(mapped_area*membership),
             category)
 
+write_csv(tbl, file = here::here("Data", "systematic-assessment-summaries",
+                                 "south-africa-summary-RLE.csv"))
+
+
+
+tbl <- zam_list %>% 
+  left_join(za_xwalk,by=c("Ecosystem_Primary"="eco_name"))  %>% 
+  transmute(efg_code, 
+            biome_code = str_extract(efg_code, "[A-Z0-9]+"),
+            mapped_area,
+            estimated_area=sum(mapped_area*membership),
+            category) %>%
+  mutate(
+    efg_code = case_when(
+      is.na(efg_code) & BroadEcosystemGroup %in% "Abyss" ~ "M3.3",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Bay" ~ "FM1.2",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Canyon" ~ "M3.2",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Kelp forest" ~ "M1.2",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Sandy shore" ~ "MT1.3",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Shallow soft shelf" ~ "MT1.7",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Deep soft shelf" ~ "MT1.7",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Shallow rocky shelf" ~ "MT1.6",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Deep rocky shelf" ~ "MT1.5",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Rocky and mixed shore" ~ "MT1.1",
+      is.na(efg_code) & BroadEcosystemGroup %in% "Island" ~ "MISSING",
+      TRUE ~ efg_code
+    )
+  )
+
+tbl %>% filter(is.na(efg_code)) %>% pull(BroadEcosystemGroup) %>% table
+
+write_csv(tbl, file = here::here("Data", "systematic-assessment-summaries",
+                                 "south-africa-marine-summary-RLE.csv"))
