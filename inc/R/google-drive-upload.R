889a205f4def87ab065f7247cc1dc5a055582391
Jose Rafael Ferrer-Paris <jr.ferrer.paris@gmail.com>
2023-12-05 15:22:05 +1100

    Upload systematic and strategic assessment summaries

--- inc/R/google-drive-upload.R
+++ inc/R/google-drive-upload.R
@@ -36,19 +36,25 @@ with_drive_quiet(
 # Upload systematic assessment summary tables ----
 
 ## create list of local files to upload:
-local_files <- dir(
-  here::here("Data","systematic-assessment-summaries"),
-  full.names=TRUE
-)
-local_files <- set_names(local_files, basename(local_files))
+all_files <- drive_ls("RLE-Africa", recursive = TRUE)
+for (slc_dir in c("systematic-assessment-summaries", "strategic-assessment-summaries")) {
+  local_files <- dir(
+    here::here("Data",slc_dir),
+    full.names=TRUE
+  )
+  local_files <- set_names(local_files, basename(local_files))
+  local_files <- subset(local_files, !names(local_files) %in% all_files$name )
+  if (length(local_files)>0) {
+    ## Create a folder on the Drive folder
+    target <- drive_mkdir(path = "RLE-Africa", name = slc_dir)
+    
+    ## upload all files into this folder by iterating over the local_files using purrr::map().
+    with_drive_quiet(
+      files <- map(local_files, ~ drive_upload(.x, path = target))
+    )  
+  }
+}
 
-## Create a folder on the Drive folder
-target <- drive_mkdir("RLE-Africa/systematic-assessment-summaries")
-
-## upload all files into this folder by iterating over the local_files using purrr::map().
-with_drive_quiet(
-  files <- map(local_files, ~ drive_upload(.x, path = target))
-)
 
 # List of files and removing duplicates ----
 
